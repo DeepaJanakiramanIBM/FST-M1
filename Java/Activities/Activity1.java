d55651c4c4e3bb1d74218fcb4940c1d38314dc95
DeepaJanakiramanIBM <deepa.janakiraman1@pearson.com>
2026-06-12 10:40:45 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,16 @@
+package com.activities;
+
+
+public class Activity1 
+{
+
+	public static void main(String[] args)
+	{
+	  Car car1 = new Car("Black","Automatic",2020,0,0);
+	  car1.displayCharacteristics();
+	  car1.accelarate();
+	  car1.brake();
+	    }
+	}
+
+
