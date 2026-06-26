04a26fd0c37100da53a6a03faea5096b5024745e
DeepaJanakiramanIBM <deepa.janakiraman1@pearson.com>
2026-06-26 10:36:49 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity3.java
@@ -0,0 +1,30 @@
+package Activity;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+
+	public class Activity3 {
+		public static void main(String args[]) {
+			
+			WebDriver driver = new FirefoxDriver();
+			
+			driver.get("https://training-support.net/webelements/login-form/");
+			
+			System.out.println("Title of the page: "+driver.getTitle());
+			
+			
+	        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("admin");
+
+			
+			driver.findElement(By.xpath("//input[@id='password']")).sendKeys("password");
+			
+			driver.findElement(By.xpath("//button[text()= 'Submit']")).click();
+			
+			driver.quit();
+		}
+
+	}
+
+
+
